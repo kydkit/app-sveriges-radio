ea435f3df2db47074d7648df4858eb40801c8c8e
Kyd <kyd.kitchaiya@medieinstitutet.se>
2021-04-27 22:11:24 +0200

    backend moved db out of api folder

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -2,31 +2,27 @@ const sqlite3 = require("sqlite3");
 const Encrypt = require("../Encrypt");
 const path = require("path");
 
-const db = new sqlite3.Database(path.join(__dirname, "../radio.db"));
+const db = new sqlite3.Database(path.join(__dirname, "../../radio.db"));
 
-//Route handles 
+//Route handles
 const whoami = (req, res) => {
   //if user exists we get back user. if not we get null
-  res.json(req.session.user || null); 
-}
+  res.json(req.session.user || null);
+};
 
 //user story 8, be able to log in
 const login = (req, res) => {
-  // let rootUser = {
-  //   username: "Root",
-  // };
-
+  let userToLogin = req.body;
   let query = /*sql*/ `SELECT * FROM users WHERE email = $email`;
-  let params = { $email: req.body.email };
-  // let user = null;
+  let params = { $email: userToLogin.email };
+
   db.get(query, params, (err, userInDB) => {
     if (!userInDB) {
       res.status(401).json({ error: "Bad credentials" });
       return;
     }
-
-    req.body.password = Encrypt.encrypt(req.body.password);
-    if (userInDB.password === req.body.password) {
+    userToLogin.password = Encrypt.encrypt(userToLogin.password);
+    if (userInDB.password === userToLogin.password) {
       delete userInDB.password;
       req.session.user = userInDB;
       res.json({ success: "Login successful", loggedInUser: userInDB });
@@ -40,9 +36,9 @@ const login = (req, res) => {
 
 //not a user story but should have
 const logout = (req, res) => {
-  delete req.session.user; 
-  res.json({ success: "Logout Succesful"})
-}
+  delete req.session.user;
+  res.json({ success: "Logout Succesful" });
+};
 
 //User story 7, be able to register an account
 const register = (req, res) => {
@@ -55,26 +51,28 @@ const register = (req, res) => {
     if (userExist) {
       res.status(400).json({ error: "User with that email already exists" });
       return;
+    } else {
+      userToRegister.password = Encrypt.encrypt(userToRegister.password);
+      query = /*sql*/ `INSERT INTO users (email, password) VALUES ($email, $password)`;
+      params = {
+        $email: userToRegister.email,
+        $password: userToRegister.password,
+      };
+
+      db.run(query, params, function (err) {
+        if (err) {
+          res.status(400).json({ error: err });
+          return;
+        } else {
+          res.json({
+            success: "User register successful. Please log in to continue",
+            lastID: this.lastID,
+          });
+        }
+      });
     }
-  });
-
-  userToRegister.password = Encrypt.encrypt(userToRegister.password);
-  query = /*sql*/ `INSERT INTO users (email, password) VALUES ($email, $password)`;
-  params = {
-    $email: userToRegister.email,
-    $password: userToRegister.password,
-  };
-
-  db.run(query, params, function (err) {
-    if (err) {
-      res.status(400).json({ error: err });
-      // throw new Error(err); 
-      return;
-    }
-
-    res.json({ success: "User register successful", lastID: this.lastID });
   });
 };
 
 //Export the different route handlers
-module.exports = { whoami, login, logout, register }; 
+module.exports = { whoami, login, logout, register };
