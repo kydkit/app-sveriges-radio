b69a2d4cdd778c4569e0e7489db4508c615cf1c3
Kyd <kyd.kitchaiya@medieinstitutet.se>
2021-05-10 23:57:19 +0200

    favorite programs added to favorites page

--- client/src/components/FavLoggedIn.jsx
+++ client/src/components/FavLoggedIn.jsx
@@ -3,18 +3,21 @@ import styles from "../css/FavLoggedIn.module.css";
 import { UserContext } from "../contexts/UserContext";
 import { RadioContext } from "../contexts/RadioProvider";
 import ChannelCard from "../components/ChannelCard";
-import ChannelProgramCard from "../components/ChannelProgramCard"; 
+import ChannelProgramCard from "../components/ChannelProgramCard";
 import { FavoritesContext } from "../contexts/FavoritesContext";
 
 const FavLoggedIn = () => {
-  const { user, whoami } = useContext(
-    UserContext
-  );
-  const { getUserFavChannel, userFavChannel } = useContext(FavoritesContext)
+  const { user, whoami } = useContext(UserContext);
+  const {
+    getUserFavChannel,
+    userFavChannel,
+    getUserFavProgram,
+    userFavProgram,
+  } = useContext(FavoritesContext);
   const { channels, getAllChannels, programs } = useContext(RadioContext);
   const userId = user.userId;
   const [filteredFavChannels, setFilteredFavChannels] = useState(null);
-  // const [filteredFavPrograms, setFilteredFavPrograms] = useState(null); 
+  const [filteredFavPrograms, setFilteredFavPrograms] = useState(null);
 
   useEffect(() => {
     whoami();
@@ -27,10 +30,10 @@ const FavLoggedIn = () => {
     if (channels && userFavChannel) {
       filterChannels();
     }
-    // getUserFavProgram(userId); 
-    // if (programs && userFavProgram) {
-    //   filterPrograms(); 
-    // }
+    getUserFavProgram(userId);
+    if (programs && userFavProgram) {
+      filterPrograms();
+    }
     // eslint-disable-next-line
   }, [user]);
 
@@ -39,23 +42,45 @@ const FavLoggedIn = () => {
     const filteredFavs = channels.filter((channel) =>
       favChannelIds.includes(channel.id)
     );
-
+    // console.log(filteredFavs);
     setFilteredFavChannels(filteredFavs);
   };
 
-  // const filterPrograms = () => {
-  //   const favProgramIds = userFavProgram.map((fp) => fp.programId); 
-  //   const filteredFavs = programs.filter((program) =>
-  //   favProgramIds.includes(program.id)
-  //   ); 
+  const filterPrograms = () => {
+    const favProgramIds = userFavProgram.map((fp) => fp.programId);
+    const filteredFavs = programs.filter((program) =>
+      favProgramIds.includes(program.id)
+    );
+    // console.log(filteredFavs);
+    setFilteredFavPrograms(filteredFavs);
+  };
 
-  //   setFilteredFavPrograms(filteredFavs)
-  // }
+  let favPrograms = "";
+  if (filteredFavPrograms) {
+    favPrograms = (
+      <>
+        <div className={styles.cardContainer}>
+          {filteredFavPrograms &&
+            filteredFavPrograms.map((fp) => (
+              <div className={styles.card}>
+                <img
+                  className={styles.image}
+                  src={fp.programimagewide}
+                  alt="program snips"
+                />
+                <h3 className={styles.programName}>{fp.name}</h3>
+                <p>{fp.description.slice(0, 70) + `...`}</p>
+              </div>
+            ))}
+        </div>
+      </>
+    );
+  }
 
   return (
     <div className={styles.loggedinContainer}>
       <ChannelCard channels={filteredFavChannels} />
-      {/* <ChannelProgramCard program={filteredFavPrograms}/> */}
+      {favPrograms}
     </div>
   );
 };
